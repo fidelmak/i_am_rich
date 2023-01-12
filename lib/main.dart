7378dfa5960724b08f3bb9d8b8b0659689c55b81
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-01-12 05:39:33 +0100

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,5 +7,11 @@ void main() {
           appBar: AppBar(
             title: Text("I AM RICH "),
             backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://cdn.pixabay.com/photo/2016/11/23/14/50/apple-1853337_960_720.jpg'),
+            ),
           ))));
 }
