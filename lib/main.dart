daede4bcf88ba0420e0b9a2ede5a073878e1b435
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-01-13 01:56:25 +0100

    new file:   android/app/src/main/res/mipmap-hdpi/get_rich_icon.png

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,6 @@ void main() {
             backgroundColor: Colors.blueGrey[900],
           ),
           body: Center(
-            child: Image(
-              image: NetworkImage(
-                  'https://cdn.pixabay.com/photo/2016/11/23/14/50/apple-1853337_960_720.jpg'),
-            ),
+            child: Image(image: AssetImage('images/dia.png')),
           ))));
 }
